19b9c6e560f7669406bcfe6bbc94c1d7fe82b8d0
Ivan Ladelshchikov <lia@itsirius.su>
2016-06-15 23:30:28 +0300

    changed create-svg test to make it user-frendlier

--- test/create-svg.sh
+++ test/create-svg.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-# before you run the test, do
-# 'sudo apt-get install sqlite3 graphviz'
 
 DEMO_DIR=/tmp/demo
-rm -rf $DEMO_DIR && mkdir $DEMO_DIR
+rm -rf ${DEMO_DIR} && mkdir ${DEMO_DIR}
 
 declare -A SQLITE_SOURCES
 SQLITE_SOURCES['north']="https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/northwindextended/Northwind.Sqlite3.sql"
@@ -13,12 +11,14 @@ SQLITE_SOURCES['chinook']="https://raw.githubusercontent.com/lerocha/chinook-dat
 
 for i in "${!SQLITE_SOURCES[@]}"; do
 	sql="${SQLITE_SOURCES[$i]}"
-    echo $i
+    echo -n "${i} - "
     # remove unicode and other marks from downloaded source
     # insert transaction mechanics
-    cat <(echo "BEGIN TRANSACTION;"; curl -s $sql | sed '1s/^\xEF\xBB\xBF//' ; echo -e "\nEND TRANSACTION;") > $DEMO_DIR/$i.sql
+    cat <(echo "BEGIN TRANSACTION;"; curl -s ${sql} | sed '1s/^\xEF\xBB\xBF//' ; echo -e "\nEND TRANSACTION;") > ${DEMO_DIR}/${i}.sql
     # make the database
-    sqlite3 $DEMO_DIR/$i.db3 <$DEMO_DIR/$i.sql > /dev/null
-    # and finally get the svg from it
-    python pydbv/model.py sqlite:///$DEMO_DIR/$i.db3 | tee $DEMO_DIR/$i.gv | dot -Kdot -Tsvg > $DEMO_DIR/$i.svg
+    sqlite3 ${DEMO_DIR}/${i}.db3 <${DEMO_DIR}/${i}.sql > /dev/null
+    # and finally get the SVG out of it
+    python pydbv/model.py sqlite:///${DEMO_DIR}/${i}.db3 | tee ${DEMO_DIR}/${i}.gv | dot -Kdot -Tsvg > ${DEMO_DIR}/${i}.svg
+
+    echo file://${DEMO_DIR}/${i}.svg
 done
