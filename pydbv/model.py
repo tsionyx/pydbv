a44fcf81540e461c50b12efd1e9b2980fbcc6bfa
Ivan Ladelshchikov <lia@itsirius.su>
2016-06-15 22:49:12 +0300

    clean from dirt

--- pydbv/model.py
+++ pydbv/model.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
-
-'''
+# -*- coding: utf-8 -*-
+"""
 Gets the visual representation of database schema (data model)
-'''
+"""
+
 
 import os
 import sys
@@ -24,10 +25,10 @@ except NameError:
 
 
 def main(sqlalchemy_connection, show_type=False):
-    '''
+    """
     The main function used to generate database schema
     description in the dot language
-    '''
+    """
     engine = create_engine(sqlalchemy_connection)
 
     meta = MetaData()
@@ -39,19 +40,19 @@ def main(sqlalchemy_connection, show_type=False):
         lstrip_blocks=True)
     node_template = env.get_template('node-template.html')
 
-    G = pgv.AGraph(strict=False,
+    g = pgv.AGraph(strict=False,
                    directed=True,
                    name=engine.url.database,
                    graph_type='digraph',
                    compound='true',
                    rankdir='RL')
     tables = meta.tables.values()
-    #tables = engine.table_names()
+    # tables = engine.table_names()
 
     for table in tables:
         if table.name.lower().startswith('sqlite_'):
             continue
-        G.add_node(
+        g.add_node(
             table.name,
             label=node_template.render(
                 title=table.name,
@@ -67,7 +68,7 @@ def main(sqlalchemy_connection, show_type=False):
                 continue
 
             uniq_child = fk.parent.primary_key or fk.parent.unique
-            G.add_edge(
+            g.add_edge(
                 table.name,
                 fk.column.table.name,
                 tailport="{0}:w".format(fk.parent.name),
@@ -79,77 +80,12 @@ def main(sqlalchemy_connection, show_type=False):
                 # dir='both',
                 arrowhead='none', dir='back',
                 arrowtail='teeodot' if uniq_child else 'crowodot'
-                #samehead=fk.column.name, sametail=fk.parent.name,
+                # samehead=fk.column.name, sametail=fk.parent.name,
             )
-    return G.to_string()
+    return g.to_string()
 
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         sys.exit('usage: {0} [database-connection]')
     print main(sys.argv[1], True)
-
-
-'''
-
-def __row2dict(row):
-    dictret = dict(row.__dict__)
-    dictret.pop('_sa_instance_state', None)
-    return dict((k, v) for k, v in dictret.items())
-
-
-def multiplicity_indicator(prop):
-    if prop.uselist:
-        return ' *'
-    if hasattr(prop, 'local_side'):
-        cols = prop.local_side
-    else:
-        cols = prop.local_columns
-    if any(col.nullable for col in cols):
-        return ' 0..1'
-    if show_multiplicity_one:
-        return ' 1'
-    return ''
-
-
-def name_for_scalar_relationship(base, local_cls, referred_cls, constraint):
-    name = referred_cls.__name__.lower()
-    local_table = local_cls.__table__
-    if name in local_table.columns:
-        return name + "_"
-    return name
-
-
-__COLLECTION_SUFFIX = "_collection"
-def name_for_collection_relationship(base, local_cls, referred_cls, constraint):
-    s= "{0}_{1}_from_{2}({3})".format(
-            local_cls.__name__,# .lower(),
-            #referred_cls.__name__,# .lower(),
-            __COLLECTION_SUFFIX,
-            constraint.table.name,
-            ','.join(c.name for c in constraint.columns))
-    #pdb.set_trace()
-    #print s
-    return s
-
-
-def main2(sqlalchemy_connection):
-    engine = create_engine(sqlalchemy_connection)
-    session = Session(engine)
-    Base = automap_base()
-    Base.prepare(engine, reflect=True,
-        name_for_collection_relationship=name_for_collection_relationship,
-        name_for_scalar_relationship=name_for_scalar_relationship)
-
-    for table_name, table_class in Base.classes.items():
-        print table_name
-        for m in dir(table_class):
-            if __COLLECTION_SUFFIX in m:
-                child = getattr(table_class, m)
-                print '\t{0}, {1}, {2}'.format(child, child.property.uselist, child.property.secondary)
-        tabledata = session.query(table_class).all()
-        for row in tabledata:
-            row_dict = __row2dict(row)
-            print row_dict
-    #list(meta.tables['bar'].columns['foo'].foreign_keys)
-'''
--- setup.py
+++ setup.py
@@ -1,46 +1,24 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# pylin: disable=attribute-defined-outside-init
-'''
-    Distribution module
-'''
+# pylint: disable=attribute-defined-outside-init
 
-import sys
+"""
+    Distribution module
+"""
+
 from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
 
 
 VERSION = '0.0.1'
 
-try:
-    from pypandoc import convert
-    read_md = lambda f: convert(f, 'rst')
-except ImportError:
-    print("warning: pypandoc module not found,"
-          " could not convert Markdown to RST")
-    read_md = lambda f: open(f, 'r').read()
 
-print read_md('README.md')
-
-
-class Tox(TestCommand):
-    '''
-        Provides a way to run tox tests via './setup.py test' call
-    '''
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        # import here, cause outside the eggs aren't loaded
-        import tox
-        sys.exit(tox.cmdline(self.test_args))
-
+def read_md(f):
+    return open(f, 'r').read()
 
 # to install:
-# sudo apt-get install python-dev virtualenvwrapper pandoc pkg-config graphviz-dev
+# sudo apt-get install python-dev virtualenvwrapper pkg-config graphviz-dev
+# sudo yum install python-devel python-virtualenvwrapper pkgconfig graphviz-devel
+
+
 setup(name='pydbv',
       version=VERSION,
       description='Python database visualizer tools',
@@ -51,7 +29,4 @@ setup(name='pydbv',
       license='MIT',
       keywords='database visualize diagram',
       packages=find_packages(),
-      install_requires=['pygraphviz', 'SQLAlchemy', 'jinja2'],
-      tests_require=['tox', 'virtualenv'],
-      cmdclass={'test': Tox},
-      setup_requires=['pypandoc'])
+      install_requires=['pygraphviz', 'SQLAlchemy', 'jinja2'])
